b850c0be6240e28ddb611107e1f1a19babc07501
Rev. Brian Scott O'keefe <github@randomblink.com>
2026-01-18 10:44:01 -0600

    Add basic site pages (home, about, contact)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+  get "pages/home"
+  root "pages#home"
+  get "/about",   to: "pages#about",   as: :about
+  get "/contact", to: "pages#contact", as: :contact
+
+  get "pages/about"
+  get "pages/contact"
   # Password reset (minimal)
   get  "/password/new", to: "passwords#new",    as: :new_password
   post "/password",     to: "passwords#create", as: :password
   resource :session
   resources :posts, param: :slug
   get "/blog", to: "posts#index"
-  root "posts#index"
+  # root "posts#index"
 end
