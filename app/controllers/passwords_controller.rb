324abc64175971b36355a896c6126d305f8ec8dc
Rev. Brian Scott O'keefe <github@randomblink.com>
2026-01-18 10:23:43 -0600

    Add password reset placeholder page

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,35 +1,20 @@
-class PasswordsController < ApplicationController
-  allow_unauthenticated_access
-  before_action :set_user_by_token, only: %i[ edit update ]
-  rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_password_path, alert: "Try again later." }
+# frozen_string_literal: true
 
+class PasswordsController < ApplicationController
+  allow_unauthenticated_access only: %i[new create]
   def new
+    # Shows the reset form (minimal placeholder)
   end
 
   def create
-    if user = User.find_by(email_address: params[:email_address])
-      PasswordsMailer.reset(user).deliver_later
-    end
+    email = params[:email].to_s.strip
 
-    redirect_to new_session_path, notice: "Password reset instructions sent (if user with that email address exists)."
-  end
-
-  def edit
-  end
-
-  def update
-    if @user.update(params.permit(:password, :password_confirmation))
-      @user.sessions.destroy_all
-      redirect_to new_session_path, notice: "Password has been reset."
+    if email.present?
+      flash.now[:notice] = "If an account exists for #{email}, reset instructions will be sent."
+      render :new, status: :ok
     else
-      redirect_to edit_password_path(params[:token]), alert: "Passwords did not match."
+      flash.now[:alert] = "Please enter your email."
+      render :new, status: :unprocessable_entity
     end
   end
-
-  private
-    def set_user_by_token
-      @user = User.find_by_password_reset_token!(params[:token])
-    rescue ActiveSupport::MessageVerifier::InvalidSignature
-      redirect_to new_password_path, alert: "Password reset link is invalid or has expired."
-    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # Password reset (minimal)
+  get  "/password/new", to: "passwords#new",    as: :new_password
+  post "/password",     to: "passwords#create", as: :password
   resource :session
   resources :posts, param: :slug
   get "/blog", to: "posts#index"
